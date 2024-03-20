faf0da6089ed7ff4362be769981af631228644e0
umarfchy <umarfchy@gmail.com>
2024-03-21 01:39:30 +0600

    make path dynamic

--- proxy-server/main.go
+++ proxy-server/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	// Define the target server to proxy to
 	target := "http://localhost:8080"
 	remote, err := url.Parse(target)
 	if err != nil {
@@ -17,33 +16,37 @@ func main() {
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 
-	// Custom Director to modify the request before forwarding
+	// Modify the request before it gets forwarded to the target
 	director := proxy.Director
 	proxy.Director = func(req *http.Request) {
-		director(req) // Call the default director
+		director(req)
 
-		// Rewrite the path
-		req.URL.Path = strings.TrimPrefix(req.URL.Path, "/namespace")
-		if req.URL.Path == "" {
+		// Split the URL path and remove the first element (dynamic text/ID)
+		splitPath := strings.SplitN(req.URL.Path, "/", 3)
+		if len(splitPath) > 2 {
+			req.URL.Path = "/" + splitPath[2]
+		} else {
 			req.URL.Path = "/"
 		}
 
-		// Set headers
+		// Update the headers
 		req.Header.Set("Host", req.Host)
-		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+		req.Header.Set("X-Forwarded-Host", req.Host)
 		req.Header.Set("X-Forwarded-For", req.RemoteAddr)
-		req.Header.Set("X-Forwarded-Proto", "http")
+		req.Header.Set("X-Forwarded-Proto", req.URL.Scheme)
 		req.Header.Set("X-Real-IP", req.RemoteAddr)
 	}
 
-	// Setup the server
-	http.HandleFunc("/namespace/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.Count(r.URL.Path, "/") < 2 {
+			http.NotFound(w, r)
+			return
+		}
 		r.URL.Host = remote.Host
 		r.URL.Scheme = remote.Scheme
 		proxy.ServeHTTP(w, r)
 	})
 
-	// Start the server
 	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err)
